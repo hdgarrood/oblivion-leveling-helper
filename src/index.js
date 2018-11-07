706c5c6154f2152591f74174581a895c1620e3eb
Harry Garrood <harry@garrood.me>
2018-11-07 15:23:06 +0000

    Implement expandable tabs

--- src/index.js
+++ src/index.js
@@ -6,23 +6,43 @@ import * as lib from './lib.js';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = this.readStateFromLocalStorage() || lib.newAdvancements();
+    this.state = this.readStateFromLocalStorage() || this.initialState();
   }
 
-  handleClick(skill) {
-    this.setState((state, props) => lib.advance(state, skill));
+  initialState() {
+    const tabs = lib.attributes
+      .filter((attr) => attr !== 'Luck')
+      .map((attr) => ({ name: attr, open: true }));
+
+    return {
+      advancements: lib.newAdvancements(),
+      tabs: tabs
+    };
+  }
+
+  handleAdvance(skill) {
+    this.setState((state, props) =>
+      ({ advancements: lib.advance(state.advancements, skill) }));
+  }
+
+  toggleTab(attr) {
+    this.setState((state, props) =>
+      ({ tabs: state.tabs.map((tab) =>
+        tab.name === attr ?
+          { name: attr, open: !tab.open } :
+          tab)}))
   }
 
   handleReset() {
-    this.setState(lib.newAdvancements());
+    this.setState(this.initialState());
   }
 
   readStateFromLocalStorage() {
-    return JSON.parse(window.localStorage.getItem('advancements'));
+    return JSON.parse(window.localStorage.getItem('ui-state'));
   }
 
   saveStateToLocalStorage() {
-    window.localStorage.setItem('advancements', JSON.stringify(this.state));
+    window.localStorage.setItem('ui-state', JSON.stringify(this.state));
   }
 
   componentDidUpdate() {
@@ -30,40 +50,41 @@ class App extends React.Component {
   }
 
   renderSkill(skill) {
-    const advancedBy = this.state[skill];
+    const advancedBy = this.state.advancements[skill];
     const info = (advancedBy === 0) ? '0' : '+' + advancedBy;
     return (
       <li key={skill}>
         <span className="skill-name">{skill}</span>
         <span className="skill-levels">{info}</span>
-        <button onClick={() => this.handleClick(skill)}>Advance</button>
+        <button onClick={() => this.handleAdvance(skill)}>Advance</button>
       </li>
     );
   }
 
   getBonusFor(attr) {
-    return lib.attributeBonus(this.state, attr);
+    return lib.attributeBonus(this.state.advancements, attr);
   }
 
-  renderAttribute(attr) {
+  renderAttributeTab(tab) {
+    const attr = tab.name;
     const bonus = this.getBonusFor(attr);
-    if (attr === 'Luck') {
-      return;
-    } else {
-      return (
-        <li key={"attr-" + attr} className="attr">
-          <div className="attr-header">
-            <span className="attr-name">{attr}</span>
-            <span className="attr-bonus">+{bonus.bonus}</span>
-          </div>
+    return (
+      <li key={"attr-" + attr} className={'attr tab-' + (tab.open ? 'open' : 'closed')}>
+        <div className="attr-header" onClick={() => this.toggleTab(tab.name)}>
+          <span className="attr-name">{attr}</span>
+          { tab.open ? <span className="attr-bonus">+{bonus.bonus}</span> : null}
+          <span className="expand-arrow">{ tab.open ? '△' : '▽' }</span>
+        </div>
+        { tab.open ?
           <div className="attr-body">
             <ul className="skills">
               {lib.skillsGovernedBy(attr).map((skill) => this.renderSkill(skill))}
             </ul>
-          </div>
-        </li>
-      );
-    }
+          </div> :
+          null
+        }
+      </li>
+    );
   }
 
   render() {
@@ -71,7 +92,7 @@ class App extends React.Component {
       <div>
         <h1>Oblivion leveling helper</h1>
         <ul className="attributes">
-          {lib.attributes.map((attr) => this.renderAttribute(attr))}
+          {this.state.tabs.map((tab) => this.renderAttributeTab(tab))}
         </ul>
         <div className="extra-controls">
           <button onClick={() => this.handleReset()}>Reset</button>
